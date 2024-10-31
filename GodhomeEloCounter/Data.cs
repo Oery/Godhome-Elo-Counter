3b43f2c7ba90d5d546a94b210d34eb2c6a8cc2df
Oery <48154940+Oery@users.noreply.github.com>
2024-10-31 18:08:10 +0100

    Fix Boss can be null

--- GodhomeEloCounter/Data.cs
+++ GodhomeEloCounter/Data.cs
@@ -9,14 +9,18 @@ namespace GodhomeEloCounter {
         public List<Boss> bosses = new List<Boss>();
 
         public void UpdateBoss(string sceneName, bool hasWon, TimeSpan timeSpan) {
+            Boss boss = FindOrCreateBoss(sceneName);
+            boss.Update(hasWon, timeSpan);
+        }
+
+        public Boss FindOrCreateBoss(string sceneName) {
             foreach (Boss boss in bosses) {
                 if (boss.sceneName == sceneName) {
-                    boss.Update(hasWon, timeSpan);
-                    return;
+                    return boss;
                 }
             }
             bosses.Add(new Boss() { sceneName = sceneName, elo = 0, streak = 0, bestWinStreak = 0, wins = 0, losses = 0, timeSpent = TimeSpan.Zero });
-            bosses.Last().Update(hasWon, timeSpan);
+            return bosses.Last();
         }
     }
 }
