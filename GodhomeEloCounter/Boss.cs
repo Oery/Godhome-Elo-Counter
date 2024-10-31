1a5b83df790051ccbb7f168208d781ac7e324fa0
Oery <48154940+Oery@users.noreply.github.com>
2024-10-31 15:04:22 +0100

    Add Boss Data Storage

---
+++ GodhomeEloCounter/Boss.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace GodhomeEloCounter
+{
+	[Serializable]
+	public class Boss
+	{
+		public string sceneName;
+
+		public int elo;
+
+		public int streak;
+		public int bestWinStreak;
+
+		public int wins;
+		public int losses;
+
+		public TimeSpan timeSpent;
+
+		public void Update(bool hasWon, TimeSpan timeSpan) {
+			this.UpdateTime(timeSpan);
+			this.UpdateWins(hasWon);
+			this.UpdateStreak(hasWon);
+			this.UpdateELO(hasWon);
+		}
+
+		private void UpdateWins(bool hasWon)
+		{
+			if (hasWon) { wins++; }
+			else { losses++; }
+		}
+
+		private void UpdateStreak(bool hasWon)
+		{
+			if (hasWon)
+			{
+				if (streak > 0) { streak++; }
+				else { streak = 1; }
+			}
+
+			else
+			{
+				if (streak < 0) { streak--; }
+				else { streak = -1; }
+			}
+
+			if (streak > bestWinStreak) { bestWinStreak = streak; }
+		}
+
+		private void UpdateELO(bool hasWon)
+		{
+			if (hasWon) { elo += 1; }
+			else { elo -= 1; }
+		}
+
+		private void UpdateTime(TimeSpan timeSpan) { timeSpent += timeSpan; }
+	}
+}
+
